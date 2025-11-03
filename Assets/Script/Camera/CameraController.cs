3d8d8e291b3747ea52d375fe3d9b02adc024955a
TouhruCreative <touhru.shott@gmail.com>
2025-11-03 19:50:31 +0500

    Update Camera Control
    Update camera: add PosMove to Input Action Asset
    moving using the edges of the screen

--- Assets/Script/Camera/CameraController.cs
+++ Assets/Script/Camera/CameraController.cs
@@ -2,34 +2,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust this value to change camera movement speed
+    
     [SerializeField] private InputActionAsset CustomInput;
-     private InputAction moveAction;
+    private InputAction moveAction;
+    private InputAction moveActionMouse;
+    private const float EDGE_THRESHOLD = 0.4f;//a variable indicating how far the camera will move to the end of the screen.
+    public float moveSpeed = 5f;//Speed Camera
     private void OnEnable()
     {
-        // Assuming you have an action map called "Player" and an action called "Move" in your asset
         var map = CustomInput.FindActionMap("Player");
         moveAction = map.FindAction("Move");
+        moveActionMouse = map.FindAction("MousePos");
         moveAction.Enable();
     }
-
     private void OnDisable()
     {
         moveAction.Disable();
     }
     void Update()
     {
-
+        Vector2 mousepos = moveActionMouse.ReadValue<Vector2>();
+        Vector2 screenUV = new Vector2(mousepos.x / Screen.width - .5f, mousepos.y / Screen.height - .5f);
+        Vector3 move = Vector3.zero;
+        if (screenUV.x < -EDGE_THRESHOLD) move.x = -1f;
+        if (screenUV.x > EDGE_THRESHOLD)  move.x =  1f;
+        if (screenUV.y < -EDGE_THRESHOLD) move.y = -1f;
+        if (screenUV.y > EDGE_THRESHOLD)  move.y =  1f;
         Vector2 movementInput = moveAction.ReadValue<Vector2>();
+        
+        
 
-        // Convert to Vector3 for movement on the x and y axis (or z if desired)
         Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0f);
-
-        if (movement.magnitude > 1f)
-        {
-            movement.Normalize();
-        }
-
-        transform.position += movement * moveSpeed * Time.deltaTime;
+        transform.Translate(Time.deltaTime * (move+movement) * moveSpeed);
     }
 }
