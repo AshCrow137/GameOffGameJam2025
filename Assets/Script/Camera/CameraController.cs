44944e1546e9ae41fae8ec7a574486b10bdf1d2e
TouhruCreative <touhru.shott@gmail.com>
2025-11-04 19:35:58 +0500

    Changed in player camera Input Action Asset

--- Assets/Script/Camera/CameraController.cs
+++ Assets/Script/Camera/CameraController.cs
@@ -6,13 +6,13 @@ public class CameraController : MonoBehaviour
     [SerializeField] private InputActionAsset CustomInput;
     private InputAction moveAction;
     private InputAction moveActionMouse;
-    private const float EDGE_THRESHOLD = 0.4f;//a variable indicating how far the camera will move to the end of the screen.
+    private const float EDGE_THRESHOLD = 0.45f;//a variable indicating how far the camera will move to the end of the screen.
     public float moveSpeed = 5f;//Speed Camera
     private void OnEnable()
     {
-        var map = CustomInput.FindActionMap("Player");
-        moveAction = map.FindAction("Move");
-        moveActionMouse = map.FindAction("MousePos");
+        var map = CustomInput.FindActionMap("InGame");
+        moveAction = map.FindAction("MoveCamera");
+        moveActionMouse = map.FindAction("MoveCameraWithMouse");
         moveAction.Enable();
     }
     private void OnDisable()
@@ -29,9 +29,7 @@ public class CameraController : MonoBehaviour
         if (screenUV.y < -EDGE_THRESHOLD) move.y = -1f;
         if (screenUV.y > EDGE_THRESHOLD)  move.y =  1f;
         Vector2 movementInput = moveAction.ReadValue<Vector2>();
-        
-        
-
+        // the final calculation of the movement vector and the movement itself
         Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0f);
         transform.Translate(Time.deltaTime * (move+movement) * moveSpeed);
     }
