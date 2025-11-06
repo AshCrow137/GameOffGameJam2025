92fc9c985d785b0a83f544e087cef17c15d5858a
Rodrigo Martins de Oliveira <r.rodrigomoliveira@gmail.com>
2025-11-06 05:39:47 -0300

    Implements the Madness System
    I implements the madness System and a test for this, i implement a interface and now the Base kingdom implements the IMadnessable interface and your respective methods

--- Assets/Script/BaseKingdom.cs
+++ Assets/Script/BaseKingdom.cs
@@ -2,12 +2,63 @@ using UnityEngine;
 using System.Collections.Generic;
 
 // Base kingdom class
-public class BaseKingdom : Entity
+public class BaseKingdom : Entity, IMadnessable
 {
     Resource currentResources = new Resource();
     List<HexTile> occupiedTiles = new();
     List<BaseGridUnitScript> controlledUnits = new();
     List<HexTile> visibleTiles = new();
     Dictionary<AIKingdom, int> relationsWithOtherKingdoms = new();
- 
+
+    public float madnessLevel { get; private set; } = 0f;
+    [SerializeField]
+    public float maxMadnessLevel { get; private set; } = 100f;
+
+    public void IncreaseMadness(float amount)
+    {
+        madnessLevel += amount;
+        if(madnessLevel > maxMadnessLevel)
+        {
+            madnessLevel = maxMadnessLevel;
+        }
+    }
+
+    public void DecreaseMadness(float amount)
+    {
+        madnessLevel -= amount;
+        if(madnessLevel < 0f)
+        {
+            madnessLevel = 0f;
+        }
+    }
+
+    public MadnessEffect GetMadnessEffects()
+    {
+        if(madnessLevel == MadnessCosts.PreventAttacks)
+        {
+            DecreaseMadness(MadnessCosts.PreventAttacks);
+            return MadnessEffect.PreventAttacks;
+        }
+        else if(madnessLevel >= MadnessCosts.ImproveUnits)
+        {
+            DecreaseMadness(MadnessCosts.ImproveUnits);
+            return MadnessEffect.ImproveUnits;
+        }
+        else if(madnessLevel >= MadnessCosts.FreeUnits)
+        {
+            DecreaseMadness(MadnessCosts.FreeUnits);
+            return MadnessEffect.FreeUnits;
+        }
+        else if(madnessLevel >= MadnessCosts.BetterDeal)
+        {
+            DecreaseMadness(MadnessCosts.BetterDeal);
+            return MadnessEffect.BetterDeal;
+        }
+        else
+        {
+            return MadnessEffect.None;
+        }
+    }
+
+
 }
