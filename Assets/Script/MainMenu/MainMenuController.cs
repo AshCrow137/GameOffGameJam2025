4d9818ae3354789877aa5a7b96b03b8dee24e28f
TouhruCreative <touhru.shott@gmail.com>
2025-11-06 20:49:19 +0500

    MainMenu Update!
    We can control Main menu with keyboard: W S A D + enter

--- Assets/Script/MainMenu/MainMenuController.cs
+++ Assets/Script/MainMenu/MainMenuController.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
 public class MainMenuController : MonoBehaviour
 {
     private bool PanelOpenSounds = false;
-    private bool PanelOpenSettings =false;
+    private bool PanelOpenSettings = false;
+    [SerializeField] private InputActionAsset CustomInput;
+    private InputAction moveAction;
     [SerializeField] private GameObject PanelSounds;
     [SerializeField] private GameObject PanelSettings;
-    [SerializeField] private GameObject[] Buttons;
+    [SerializeField] private Button[] Buttons;
+    private int selectBut;
     public void Initialize()
     {
+        selectBut = 0;
         PanelSounds.SetActive(false);
+        PanelSettings.SetActive(false);
+        Buttons[0].Select();
     }
     void Start()
     {
         Initialize();
     }
+    private void OnEnable()
+    {
+        var map = CustomInput.FindActionMap("InMenu");
+        moveAction = map.FindAction("Navigate");
+        moveAction.Enable();
+    }
+    private void OnDisable() {
+        moveAction.Disable(); 
+        }
     public void btn_StartGame(string Scene)
     {
         SceneManager.LoadSceneAsync(Scene);
@@ -38,6 +57,18 @@ public class MainMenuController : MonoBehaviour
     }
     void Update()
     {
-        button.Focus();
+
     }
+    // private void OnNavigateStart(InputAction.CallbackContext context)
+    // {
+    //     // selectBut += (int)context.ReadValue<Vector2>().y;
+    //     // if (selectBut < 0)
+    //     //     selectBut = 0;
+    //     // else if (selectBut >= Buttons.Length)
+    //     //     selectBut = Buttons.Length - 1;
+
+    //     // Buttons[selectBut].Select();  
+    //     // Debug.Log(selectBut);
+
+    // }
 }
