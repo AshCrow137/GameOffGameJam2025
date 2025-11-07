9c8ea7cb0a944516c222b1620c28b9a5bb83870b
Rodrigo Martins de Oliveira <r.rodrigomoliveira@gmail.com>
2025-11-07 12:10:17 -0300

    test diplomacy

--- Assets/Script/Kingdom/PlayerKingdom.cs
+++ Assets/Script/Kingdom/PlayerKingdom.cs
@@ -3,10 +3,5 @@ using UnityEngine;
 // Player controlled kingdom class
 public class PlayerKingdom : BaseKingdom
 {
-
-    public MadnessEffect GetMadnessEffects()
-    {
-        return MadnessEffect.None;
-    }
    
 }
