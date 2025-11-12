cd7717d2a90e55a3c3e64eaaed01457d8cd37178
Sayantan Pal <sayantanpal2000.2018@gmail.com>
2025-11-12 22:08:58 +0530

    .

--- Assets/Script/VisionSystem/VisionManager.cs
+++ Assets/Script/VisionSystem/VisionManager.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 public class VisionManager : MonoBehaviour
 {
     private Dictionary<Vector3Int, TileState> visionDictionary = new Dictionary<Vector3Int, TileState>();
+    public void Initialize()
+    {
+        
+    }
 }
 
