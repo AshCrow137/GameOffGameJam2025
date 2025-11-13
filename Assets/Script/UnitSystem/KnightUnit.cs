d9535f6afa8098580077e36059f738a95ce21924
Kostiantyn Slobodnyi <113297735+Kostia307@users.noreply.github.com>
2025-11-13 09:04:20 +0100

    added fishman

--- Assets/Script/UnitSystem/KnightUnit.cs
+++ Assets/Script/UnitSystem/KnightUnit.cs
@@ -5,28 +5,19 @@ public class KnightUnit : BaseGridUnitScript
 	[Header("Special ability")]
 	[SerializeField]
 	protected bool chargeActive = true;
+	[SerializeField]
+	protected int chargeDamage = 5; 
 
-	protected override void Attack(BaseGridUnitScript targetUnit)
+    protected override void Attack(BaseGridUnitScript targetUnit)
 	{
-		int currentAttackDamage = AttackDamage;
-
 		// Adding +2 damage if moved >= 3 tiles
+		// TODO: update system
 		if (chargeActive && tilesRemain <= 2)
 		{
-			currentAttackDamage += 2;
+			MeleeAttackDamage += chargeDamage;
 		}
 
-		// Multiplying damage by 1.5 if target is an archer
-		if (targetUnit.GetUnitType() == UnitType.Archer)
-		{
-			currentAttackDamage = Mathf.RoundToInt(currentAttackDamage * 1.5f);
-		}
-
-		targetUnit.TakeDamage(currentAttackDamage);
-		if (!CanMoveAfterattack)
-		{
-			tilesRemain = 0;
-			remainMovementText.text = tilesRemain.ToString();
-		}
+		base.Attack(targetUnit);
+		MeleeAttackDamage = 15;
 	}
 }
