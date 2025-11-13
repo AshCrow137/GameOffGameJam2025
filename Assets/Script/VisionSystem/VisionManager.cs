390d04bf9554b122a490bde79cf432087b676163
Sayantan Pal <sayantanpal2000.2018@gmail.com>
2025-11-13 22:16:27 +0530

    done

--- Assets/Script/VisionSystem/VisionManager.cs
+++ Assets/Script/VisionSystem/VisionManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 /// </summary>
 public class VisionManager : MonoBehaviour
 {
-    private Dictionary<Vector3Int, Fog> visionDictionary = new Dictionary<Vector3Int, Fog>();
+    // private Dictionary<Vector3Int, Fog> visionDictionary = new Dictionary<Vector3Int, Fog>();
     private Dictionary<Vector3Int, int> NotGreyFog = new Dictionary<Vector3Int, int>();
     private Dictionary<Vector3Int, bool> notBlackFog = new Dictionary<Vector3Int, bool>();
 
